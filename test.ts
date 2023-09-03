341b3049412152b1e1af35d772d05a30103e9ff3
donovan-maker <71453918+donovan-maker@users.noreply.github.com>
2023-09-03 12:44:52 -0400

    Added the extention's code

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+Binary.drawBinary(0, 1)
+Binary.drawBinary(1, 2)
+Binary.drawBinary(2, 3)
+Binary.drawBinary(3, 4)
+Binary.drawBinary(4, 5)
+basic.pause(2000)
+basic.clearScreen()
+Binary.drawBinary(0, 6)
+Binary.drawBinary(1, 7)
+Binary.drawBinary(2, 8)
+Binary.drawBinary(3, 9)
+Binary.drawBinary(4, 0)
